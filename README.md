8e17f3a8f8a1d05246e162a35c390b8555150e5d
gcarey <gcarey@vaynermedia.com>
2016-01-10 21:42:30 -0500

    Add authentication and login views

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # sails_base
 
 a [Sails](http://sailsjs.org) application
+
+## Heroku
+* __Heroku set remote__: ```$ heroku git:remote -a <heroku_repo_name>```
+* __Heroku push__: ```$ heroku push origin master```
--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,10 +8,53 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
+var crypto    = require('crypto');
 
 module.exports.bootstrap = function(cb) {
 
+  admin_username = sails.config.permissions.adminUser.username
+  admin_email = sails.config.permissions.adminUser.email
+  admin_password = sails.config.permissions.adminUser.password
+
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+  User.findOrCreate(
+    {username: admin_username},
+    {username: admin_username, email: admin_email})
+  .exec(
+    function(err, user){
+      if (err){
+          if (err.code === 'E_VALIDATION') {
+            if (err.invalidAttributes.email) {
+              console.log('error: Error.Passport.Email.Exists');
+            } else {
+              console.log('error: Error.Passport.User.Exists');
+            }
+          }
+          return cb(err)
+      }
+      var token = crypto.randomBytes(48).toString('base64');
+      Passport.findOrCreate(
+        {user: user.id},
+        {protocol: 'local', password: admin_password, user: user.id, accessToken : token})
+      .exec(function(err, passport){
+        if (err) {
+          if (err.code === 'E_VALIDATION') {
+            console.log('error: ', 'Error.Passport.Password.Invalid');
+          }
+          return user.destroy(function (destroyErr) {
+            cb(destroyErr || err);
+          });
+        }
+
+        cb();
+        sails.services.passport.loadStrategies();
+      });
+    });
 };
+
+// Passport.create({
+//       protocol    : 'local'
+//     , password    : password
+//     , user        : user.id
+//     , accessToken : token
